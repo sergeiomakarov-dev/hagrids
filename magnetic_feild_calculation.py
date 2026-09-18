Fixed additional term with correct derivative.

magnetic_feild_calculation.py
@@ -69,7 +69,7 @@ def Bvartheta_perturbation(r, vartheta, varphi, m, A, iota_res, R, varepsilon, p
         elif flag_type_A_fun == 1:
             floor_arg = 1 - k + k * (np.pi * r**2 * B0) / psi_0
             step = np.heaviside(floor_arg, 1.0)
-            additional_term = (A / (2 * np.pi * r * R)) * (k * (2 * np.pi * r * B0) / psi_0) * np.cos(phase) * step
+            additional_term = (A * k * r * B0 / (psi_0 * R)) * np.cos(phase) * step   # fixed
             Bvartheta_pert = additional_term
         else:
             raise ValueError("Invalid value for flag_type_A_fun. Must be 0 or 1.")
@@ -82,7 +82,7 @@ def Bvartheta_perturbation(r, vartheta, varphi, m, A, iota_res, R, varepsilon, p
             floor_arg = 1 - k + k * (np.pi * r**2 * B0) / psi_0
             step = np.heaviside(floor_arg, 1.0)
             extended_factor = base_factor * floor_arg * step
-            additional_term = (A / (2 * np.pi * r * R)) * (k * (2 * np.pi * r * B0) / psi_0) * np.cos(phase) * step
+            additional_term = (A * k * r * B0 / (psi_0 * R)) * np.cos(phase) * step   # fixed
             Bvartheta_pert = varepsilon * np.sin(vartheta) * extended_factor * np.sin(phase) + additional_term
         else:
             raise ValueError("Invalid value for flag_type_A_fun. Must be 0 or 1.")